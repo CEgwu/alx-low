bf71292ad5aea2d3577fcff25fd2225ce35c7589
CEgwu <chizobaegwu22@gmail.com>
2023-03-23 11:47:00 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 10 times
+ * Return: 10 times the number
+ */
+
+void more_numbers(void)
+{
+	int x,y;
+	for (x =0; x < 10; x++)
+	{
+	for (y = 0; y <= 14; y++)
+	{
+	if (y > 9)
+	{
+	_putchar ((y / 10) + '0');
+	}
+	_putchar((y % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
